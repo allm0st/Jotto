f080b39dfc99772c68f66d324a914a0ef4e030d0
Tiago Azevedo <tiago.azevedo@promptlyhealth.com>
2019-08-16 19:17:37 +0100

    Tests and adds code for non-zero guessed words

--- src/GuessedWords.js
+++ src/GuessedWords.js
@@ -5,6 +5,29 @@ export default function GuessedWords({ guessedWords }) {
   let content;
   if (!guessedWords.length) {
     content = <div data-test="guess-instructions">Try to guess the word!</div>;
+  } else {
+    const guessedWordsRows = guessedWords.map((word, index) => {
+      return (
+        <tr data-test="guessed-word" key={index}>
+          <td>{word.guessedWord}</td>
+          <td>{word.letterMatchCount}</td>
+        </tr>
+      );
+    });
+    content = (
+      <div data-test="guessed-words">
+        <h3>Guessed Words</h3>
+        <table>
+          <thead>
+            <tr>
+              <th>Guess</th>
+              <th>Matching Letters</th>
+            </tr>
+          </thead>
+          <tbody>{guessedWordsRows}</tbody>
+        </table>
+      </div>
+    );
   }
   return <div data-test="component-guessed-words">{content}</div>;
 }
