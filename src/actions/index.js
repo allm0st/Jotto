4c5d804c27122ec030ea2856835bde56e0a118d6
Tiago Azevedo <tiago.azevedo@promptlyhealth.com>
2019-08-19 12:27:40 +0100

    Set up guessWords tests

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,8 @@
 export const actionTypes = {
+  GUESS_WORD: 'GUESS_WORD',
   CORRECT_GUESS: 'CORRECT_GUESS',
 };
 
-export function correctGuess() {
-  return { type: actionTypes.CORRECT_GUESS };
+export const guessWord = (guessedWord) => (dispatch, getState) => {
+
 }
