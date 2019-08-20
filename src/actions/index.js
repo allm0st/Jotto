a414487597b8ab62482c3ba51f82c8842365ede1
Tiago Azevedo <tiago.azevedo@promptlyhealth.com>
2019-08-20 10:30:28 +0100

    Adds Thunk for 'guessedWords' piece of state

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,20 @@
+import { getLetterMatchCount } from '../helpers';
+
 export const actionTypes = {
   GUESS_WORD: 'GUESS_WORD',
   CORRECT_GUESS: 'CORRECT_GUESS',
 };
 
-export const guessWord = (guessedWord) => (dispatch, getState) => {
+export const guessWord = guessedWord => (dispatch, getState) => {
+  const secretWord = getState().secretWord;
+  const letterMatchCount = getLetterMatchCount(guessedWord, secretWord);
 
-}
+  dispatch({
+    type: actionTypes.GUESS_WORD,
+    payload: { guessedWord, letterMatchCount },
+  });
+
+  if (guessedWord === secretWord) {
+    dispatch({ type: actionTypes.CORRECT_GUESS });
+  }
+};
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux';
 import success from './successReducer';
 import guessedWords from './guessedWordsReducer';
+import secretWord from './secretWordReducer';
 
-export default combineReducers({ success, guessedWords });
+export default combineReducers({ success, guessedWords, secretWord });
 
