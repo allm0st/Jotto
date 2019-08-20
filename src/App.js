d17d03fdfdf7e89fee6c352d11acc632c075e632
Tiago Azevedo <tiago.azevedo@promptlyhealth.com>
2019-08-20 12:15:44 +0100

    Conenct App component (and tests)

--- src/App.js
+++ src/App.js
@@ -2,19 +2,31 @@ import React, { Component } from 'react';
 import './App.css';
 import Congrats from './Congrats';
 import GuessedWords from './GuessedWords';
+import { connect } from 'react-redux';
+import { getSecretWord } from './actions';
+import Input from './Input';
 
 class App extends Component {
   render() {
+    const { success, guessedWords } = this.props;
     return (
-      <div className="container">
+      <div className="container" data-test="component-app">
         <h1>Jotto!</h1>
-        <Congrats success={true} />
-        <GuessedWords
-          guessedWords={[{ guessedWord: 'train', letterMatchCount: 3 }]}
-        />
+        <Congrats success={success} />
+        <Input />
+        <GuessedWords guessedWords={guessedWords} />
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = ({ secretWord, success, guessedWords }) => ({
+  secretWord,
+  success,
+  guessedWords,
+});
+
+export default connect(
+  mapStateToProps,
+  { getSecretWord }
+)(App);
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow } from 'enzyme';
+import { storeFactory, findByTestAttr } from '../test/testUtils';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+const setup = (initialState = {}) => {
+  const store = storeFactory(initialState);
+  const wrapper = shallow(<App store={store} />)
+    .dive()
+    .dive();
+  return wrapper;
+};
+
+describe('<App />', () => {
+  const secretWord = 'party';
+  const initialState = {
+    secretWord,
+    success: false,
+    guessedWords: [
+      {
+        guessedWord: 'train',
+        letterMatchCount: 3,
+      },
+    ],
+  };
+  let wrapper;
+  beforeEach(() => {
+    wrapper = setup(initialState);
+  });
+  it('renders without crashing', () => {
+    const component = findByTestAttr(wrapper, 'component-app');
+    expect(component.length).toBe(1);
+  });
+  it("receives 'success'", () => {
+    const successProp = wrapper.instance().props.success;
+    expect(successProp).toBe(false);
+  });
+  it("receives 'guessWords'", () => {
+    const guessedWordsProp = wrapper.instance().props.guessedWords;
+    expect(guessedWordsProp).toEqual(initialState.guessedWords);
+  });
+  it("receives 'secretWord'", () => {
+    const secretWordProp = wrapper.instance().props.secretWord;
+    expect(secretWordProp).toBe(secretWord);
+  });
+  it("has 'getSecretWord' as a function passed via props", () => {
+    const getSecretWordProp = wrapper.instance().props.getSecretWord;
+    expect(getSecretWordProp).toBeInstanceOf(Function);
+  });
 });
